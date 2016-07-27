46faa70b7e6525850d7ead9ff85d87968a38a782
csjung <csjung0061@gmail.com>
2016-07-27 17:18:26 +0900

    글머리

---
+++ komec_admin/src/main/java/kr/komec/admin/repository/BoardCategoryRepository.java
@@ -0,0 +1,16 @@
+package kr.komec.admin.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kr.komec.admin.domain.entity.BoardCategory;;
+
+@Repository
+public interface BoardCategoryRepository extends JpaRepository<BoardCategory, Long>{
+	
+	int deleteByBoardConfigId(long boardConfigId);
+	
+	List<BoardCategory> findByBoardConfigId(long boardConfigId);
+}
